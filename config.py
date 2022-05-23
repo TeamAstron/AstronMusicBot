bbd7de99c061837697d39946ab89ad8a5a348c8e
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-23 11:56:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,45 @@
+import os
 from os import getenv
 from dotenv import load_dotenv
+from helpers.uptools import fetch_heroku_git_url
 
-
-admins = {}
 load_dotenv()
-
-
-api_id = int(getenv("api_id"))
-api_hash = getenv("api_hash")
-token = getenv("token")
-session = getenv("session")
-
-creator_id = int(getenv("creator_id"))
-creator_name = getenv("creator_name")
-creator_username = getenv("creator_username")
-
-database_url = getenv("database_url")
-bot_alive_image = getenv("bot_alive_image")
-sudoers_id = list(map(int, getenv("sudoers_id").split()))
-cmd_handler = list(getenv("cmd_handler", "/ . $ !").split())
-
-support_group = getenv("support_group")
-support_channel = getenv("support_channel")
+que = {}
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "Veez Music")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/8628c642a266a22effd8c.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/4c39fbb88932761913fff.png")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/73e10ed6e2bd32b478de6.png")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME", "Astronmusicbot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Astronassistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "AstronSupport")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TeamAstron")
+# isi dengan username kamu tanpa simbol @
+OWNER_NAME = getenv("OWNER_NAME", "AstronXd")
+# fill with your nickname
+ALIVE_NAME = getenv("ALIVE_NAME", "Astron")
+# fill with your id as the owner of the bot
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+DATABASE_URL = os.environ.get("DATABASE_URL")  # fill with your mongodb url
+# make a private channel and get the channel id
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+# just fill with True or False (optional)
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+# UPDATER CONFIG
+U_BRANCH = "main"
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+UPSTREAM_REPO = os.environ.get(
+    "UPSTREAM_REPO", "https://github.com/TeamAstron/AstronMusicBot"
+)
+HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
